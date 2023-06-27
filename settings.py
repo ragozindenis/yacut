a55aa5cae77051a93b5c129960ec178067b70ac0
ragozindenis <Ragozin3Dm@yandex.ru>
2023-06-27 16:42:44 -0300

    first

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
     FIELDS_API = ({'original': 'url', 'short': 'custom_id'})
